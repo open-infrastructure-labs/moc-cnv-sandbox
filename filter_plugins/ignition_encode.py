1affcdb7dfc869a5e643d4d141b8872f4552295a
Lars Kellogg-Stedman <lars@redhat.com>
2020-07-16 17:50:31 -0400

    fix syntax error in content-type marker

--- filter_plugins/ignition_encode.py
+++ filter_plugins/ignition_encode.py
@@ -3,7 +3,7 @@ import base64
 
 def ignition_encode(val, content_type='text/plain', charset='utf-8'):
     content = base64.b64encode(val.encode(charset)).decode()
-    return f'data:{content_type}:;charset={charset};base64,{content}'
+    return f'data:{content_type};charset={charset};base64,{content}'
 
 
 def ignition_encode_files(val, content_type='text/plain', charset='utf-8'):
